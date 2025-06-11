1618f24da61a0e5bdea9012286a162360c1e0420
starkad57 <fabien@uncode-studio.com>
2025-06-11 17:56:11 +0400

    Add services page

--- config.js
+++ config.js
@@ -7,6 +7,7 @@ export const pages = {
   'article': ['faq-accordion'],
   'reparations': ['faq-accordion','layout-reverse'],
   'reparation': ['faq-accordion','splide-slider'],
+  'services': ['faq-accordion','layout-reverse'],
   // 'about': ['animations'],
   // 'contact': ['contact-form', 'animations'],
   // 'shop': ['product-grid', 'animations']
