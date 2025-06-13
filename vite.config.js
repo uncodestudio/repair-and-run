6819c23d982a0a686f2916b62120e16168932de2
starkad57 <fabien@uncode-studio.com>
2025-06-13 17:41:44 +0400

    optimize production build - remove console logs automatically

--- vite.config.js
+++ vite.config.js
@@ -10,6 +10,12 @@ export default defineConfig({
   build: {
     outDir: 'dist',
     minify: 'terser',
+    terserOptions: {           
+      compress: {               
+        drop_console: true,     
+        drop_debugger: true
+      }
+    },
     lib: {
       entry: 'main.js',
       name: 'WebflowApp',
